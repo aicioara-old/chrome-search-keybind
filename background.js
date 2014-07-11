3987c5abd264cff7b61eb8d7bf2db7f5d97acbbc
aicioara <primulandrei@yahoo.com>
2014-07-10 20:20:32 -0700

    Replaced the omnibox with a contextmenu in order to reach the menu

--- background.js
+++ background.js
@@ -1,3 +1,16 @@
-chrome.omnibox.onInputEntered.addListener(function(text) {
-	window.location = 'options.html';
-});
+function setContextMenu(show) {
+	chrome.contextMenus.removeAll();
+	if (show) {
+		chrome.contextMenus.create({
+			"title": "Add shortcut",
+			"contexts":["all"],
+			"onclick":function(info,tab) {
+				console.log('hi');
+				// showPopup(info,tab);
+			}
+		});
+	}
+}
+
+
+setContextMenu(true);
